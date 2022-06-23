f179aead1d4fbf1a83bc0e416be70238ee28cf72
ShobhitMysoft <shobhit.mysoft@gmail.com>
2022-06-23 12:17:53 +0530

    first commit

---
+++ app/src/main/java/com/qdocs/smartschool/students/NotificationList.java
@@ -0,0 +1,33 @@
+package com.qdocs.smartschool.students;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ListView;
+import com.qdocs.smartschool.BaseActivity;
+import com.qdocs.smartschool.NotificationModel;
+import com.qdocs.smartschool.adapters.NotificationViewAdapter;
+import com.qdocs.smartschool.utils.DatabaseHelper;
+import com.qdocs.smartschool.R;
+import java.util.ArrayList;
+
+public class NotificationList extends BaseActivity  {
+    NotificationViewAdapter adapter;
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
+        View contentView = inflater.inflate(R.layout.activity_notification_list, null, false);
+        mDrawerLayout.addView(contentView, 0);
+        titleTV.setText(getApplicationContext().getString(R.string.notification));
+        listView = findViewById(R.id.user_list);
+
+        DatabaseHelper db = new DatabaseHelper(this);
+        ArrayList<NotificationModel> modelArrayList = db.GetUsers();
+        adapter = new NotificationViewAdapter(NotificationList.this, modelArrayList,listView);
+
+        listView.setAdapter(adapter);
+    }
+}
+
